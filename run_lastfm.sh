4e0c4cefdfc1ea07df06e70b2f046fcdf49a470f
bmpozo <borjamp@outlook.com>
2022-03-01 00:52:56 +0100

    add bash script to get data and run spark job

---
+++ run_lastfm.sh
@@ -0,0 +1,5 @@
+# -- Download lastfm data file
+curl http://mtg.upf.edu/static/datasets/last.fm/lastfm-dataset-1K.tar.gz | tar -xz  -C ./data/input/
+
+# -- Run spark job
+docker exec -it spark-master /spark/bin/spark-submit --master spark://spark-master:7077 /opt/spark-src/lastfm/lastfm.py
